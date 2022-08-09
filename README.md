b53285d223436567d4b1d6d216e3653dc5e340b1
iluvpy <miomanns265@gmail.com>
2022-08-09 15:47:57 +0200

    added db

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# Saltaboci
-a ticino board game called "saltaboci". resembling checkers 
+# QiChess
+a chess platform similar to chess.com 
+
+
+# Setup
+first run `yarn` to install all the dependencies, then run `yarn build` to compile the typescript.
+before running anything you need to create a `.env` file that should look like this;    
+```
+DATABASE_HOST="localhost" # or any other host
+DATABASE_USER="your db user"
+DATABASE_PASSWORD="your db password"
+DATABASE_NAME="your db name"
+``` 
+after creating the `.env` file you can finally run `yarn serve` or `yarn devServe` for development.
--- server.mjs
+++ server.mjs
@@ -1,30 +1,44 @@
 import express from "express";
-import http from "http";
-import { Server } from "socket.io";
 import path from "path";
+import mysql from "mysql";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+var db = mysql.createConnection({
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME
+});
+
+db.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+
+  console.log()
+});
 
 const app = express();
-const server = http.Server(app);
 const port = 3000;
 const host = "localhost";
-const io = new Server({
-    cors: { origin: "*"}
-});
 
-io.attach(server);
-
-//Whenever someone connects this gets executed
-io.on('connection', socket => {
-   
-});
-
-
+app.set("view-engine", "ejs");
 app.use(express.static("public"));
 
+
+function getWebPage(url) {
+    return path.join(process.cwd(), `views/${url}.ejs`);
+}
+
 app.get("/", (req, res) => {
-    res.sendFile(path.join(process.cwd(), "index.html"));
+    res.redirect("/home");
 });
 
-server.listen(port, host, () => {
+app.get("/home", (req, res) => {
+    res.render(getWebPage("home"));
+});
+
+app.listen(port, host, () => {
     console.log(`Example app listening at http://${host}:${port}`);
 });
